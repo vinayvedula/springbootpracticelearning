87f4c280ddccbcc9058250ccedefaa052699b331
vinayvedula <vinaykumar_vedula@yahoo.co.in>
2023-03-04 08:16:31 -0600

    "[Initial Commit]"

---
+++ practiceLearning/practicelearning/src/main/java/com/vinay/springboot/example/practicelearning/PracticelearningApplication.java
@@ -0,0 +1,13 @@
+package com.vinay.springboot.example.practicelearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticelearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticelearningApplication.class, args);
+	}
+
+}
---
+++ practiceLearning/practicelearning/src/test/java/com/vinay/springboot/example/practicelearning/PracticelearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinay.springboot.example.practicelearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticelearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
